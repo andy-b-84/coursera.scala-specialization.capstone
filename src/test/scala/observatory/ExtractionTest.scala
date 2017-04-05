037da2d8b23daddd6c7e94a2526abdc1bb39f322
Patrick Laxton <p.laxton@kreactive.com>
2017-04-05 21:47:21 +0200

    eeer... Yeah, .head is better than .take(1).map{...} ^^

--- src/test/scala/observatory/ExtractionTest.scala
+++ src/test/scala/observatory/ExtractionTest.scala
@@ -9,12 +9,11 @@ class ExtractionTest extends FunSuite {
   test("locateTemperatures works") {
     val located1975 = Extraction.locateTemperatures(1975, "/stations.csv", "/1975.csv")
 
-    located1975.take(1).map{ tuple =>
-      //println(tuple)
-      assert(tuple._1.toString == "1975-01-01")
-      assert(tuple._2.toString == "Location(70.933,-8.667)")
-      assert(tuple._3 == 23.2)
-    }
+    val tuple = located1975.head
+    //println(tuple)
+    assert(tuple._1.toString == "1975-01-01")
+    assert(tuple._2.toString == "Location(70.933,-8.667)")
+    assert(tuple._3 == 23.2)
   }
   
 }
